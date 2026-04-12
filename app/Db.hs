55ff2ca901bf3618cca6495543d2cc05afe4a2ce
LUNRA <nicolaslarra96@gmail.com>
2026-04-12 11:55:23 +0100

    get support

--- app/Db.hs
+++ app/Db.hs
@@ -1,12 +1,28 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module Db where
 
 import qualified Data.ByteString.Lazy as LBS
+import qualified Data.ByteString.Lazy.Char8 as LBSC
 import Data.Aeson
 import Data.Aeson.Key (fromString)
+import qualified Data.Aeson.KeyMap as KM
+import Data.Maybe (listToMaybe, mapMaybe)
+import Control.Exception (handle, IOException)
 
 db_set :: ToJSON v => String -> v -> IO()
 db_set k value = do
-  LBS.appendFile "db.txt" encodedInput
+  LBS.appendFile "db.txt" (encodedInput <> LBSC.pack "\n")
   where
     encodedInput = encode $ object [key .= value]
+    key = fromString k
+
+db_get :: FromJSON v => String -> IO (Maybe v)
+db_get k = handle (\(_ :: IOException) -> return Nothing) $ do
+  contents <- LBSC.readFile "db.txt"
+  let linesOfDb = LBSC.lines contents
+      -- Parse each line into an Object, then look for the key
+      objects = mapMaybe decode linesOfDb
+      values = mapMaybe (KM.lookup key) objects
+  return $ listToMaybe $ reverse values
+  where
     key = fromString k
--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,17 @@
 module Main where
 import Db
 import System.Environment (getArgs)
+import Data.Aeson (Value)
 
 main :: IO ()
 main = do
   args <- getArgs
   case args of
-    [key, value] -> db_set key value
-    _ -> putStrLn "Usage: app <key> <value>"
+    ["set", key, value] -> db_set key value
+    ["get", key] -> do
+      val <- db_get key :: IO (Maybe Value)
+      case val of
+        Just v -> print v
+        Nothing -> putStrLn "Key not found"
+    _ -> putStrLn "Usage: \n  bitbarrel set <key> <value>\n  bitbarrel get <key>"
 
